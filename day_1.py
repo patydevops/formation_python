a24b2bdd8427bec1713c3b48bab8775f07b38b26
patricia <jalennenjapa@gmail.com>
2023-04-03 14:28:13 -0400

    avancer

--- day_1.py
+++ day_1.py
@@ -26,7 +26,7 @@ if nombre%2==0:
 else:
     print(nombre, " est impair.")
     """ merci pour ce first day
-    """=
+    """
     
     
     """exercice 1: calcul de moyenne"""
@@ -104,3 +104,87 @@ if nombre > 1:
         
 else:
     print(nombre, "n'est pas premier")
+    
+    
+""" exercice:8 Somme
+"""
+a= float(input("entre le premier nombre:"))
+b= float(input("entre le second nombre:"))
+S= a+b
+print('la somme de ces nombres est:',S)
+
+
+""" exercice:9 Le Tri des visteurs
+"""
+age=int(input("entre ton age:"))
+if age >= 18:
+  print("Majeur") 
+else:
+  print("Mineur")  
+    
+  
+""" exercice:10 Mes Diviseurs
+"""    
+nombre = int(input("entre un nombre entier:"))
+while nombre <= 0:
+    print("error")
+    nombre = int(input("entre un nombre entier:"))
+print("les diviseurs de ce nombre sont:")
+for i in range(1, nombre+1):
+ if nombre % i == 0:
+    print(i)
+    
+
+""" exercice:10 Le PGCD
+"""          
+x= int(input("entre le premier nombre entier:"))
+y= int(input("entre le second nombre entier:"))
+while x <= 0:
+     print("error")
+     x = int(input("entre le premier nombre entier:"))
+while y <= 0:
+     print("error")
+     y = int(input("entre le second nombre entier:"))    
+if x > y :
+    min = y
+else :
+    min = x
+for i in range (1, min+1):
+       if x % i == 0 and y % i == 0:
+           pgcd = i
+print("le pgcd de ces deux nombres est:", pgcd)    
+    
+    
+""" exercice:11 Le Tri
+"""    
+nombres= input("saisir une liste de nombres:")
+liste=[int(nombre)for nombre in nombres.split()]
+print(liste)
+trie=sorted(liste)
+print(trie)
+
+
+""" exercice:12 Annee bissextile
+"""     
+annee= int(input("saisir votre annee:"))
+if annee % 4 == 0 and (annee % 100 != 0 or annee % 400 == 0):
+    print(annee, "est bissextile")
+else:
+    print(annee, "n'est pas bissextile")
+    
+ 
+""" exercice:13 Mon occurence
+""" 
+chaine= input("saisissez une chaine de caractere:")
+matrice= {}
+for caractere in chaine:
+    if caractere in matrice:
+        matrice[caractere]+=1
+    else:
+        matrice[caractere]=1
+for caractere,occurence in matrice.items():
+    print(caractere+"-"+ str(occurence))
+        
+    
+
+
